7881c1317e2503ea28fa185a27959d0ceb1fd097
islamajabul <ajabulmd1@gmail.com>
2023-02-21 22:44:53 +0800

    class four

---
+++ js/template.js
@@ -0,0 +1,27 @@
+const person = 'Adam Khan';
+const person2 = "Rinku Khan";
+const person3 = `Rimon khan`;
+
+const multiLine = 'First line of text \n' + 
+'Second line of text' + 
+'third line of text';
+// console.log(multiLine);
+
+const newMultiLine = `First line of string
+Second line of string
+Third line of string`;
+// console.log(newMultiLine);
+
+const a = 20;
+const b = 30;
+const nums = [78, 98, 33, 58, 69, 6, 444]
+
+const summary = 'sum of: ' + a + ' and ' + b + ' is ' + (a+b);
+// console.log(summary);
+
+const newSummary = `sum of ${a} and ${b} is: ${a+b}`;
+console.log(newSummary);
+
+const newSummary2 = `sum of ${a} and ${nums.length} is: ${a+nums.length}`;
+// console.log(newSummary);
+

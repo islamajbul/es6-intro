b82855ef4bc4a059a6d178c35961caeed551723d
islamajabul <ajabulmd1@gmail.com>
2023-02-22 12:01:23 +0800

    arrow function

---
+++ js/arrow.js
@@ -0,0 +1,14 @@
+/* function add(first, second = 0) {
+    const total = first + second;
+    return total;
+} */
+// Function expression with anonymous function
+const add1 = function add1(first, second){
+    const total = first + second;
+    return total;
+}
+
+
+const result = add(10, 20);
+// console.log(result);
+// console.log(add1);
